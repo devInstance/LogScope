788c13723e3ff9bbf600752d4590b7fd250a2d2c
yfranz <yfranz@gmail.com>
2021-01-22 10:17:00 -0800

    Adding service collection extensions to logging

---
+++ LogScope/Extensions/WebAssemblyServiceExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevInstance.LogScope.Extensions
+{
+    public static class WebAssemblyServiceExtension
+    {
+        public static IServiceCollection AddConsoleLogging(this IServiceCollection col, LogLevel level)
+        {
+            return col.AddSingleton<ILogProvider>(new ConsoleLogProvider(level));
+        }
+
+        public static IServiceCollection AddLoggingProvider(this IServiceCollection col, ILogProvider provider)
+        {
+            return col.AddSingleton(provider);
+        }
+    }
+}
