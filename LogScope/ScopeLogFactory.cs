9fb3c475310c63ab72b70cbe5243ba86c6de87b6
yfranz <yfranz@gmail.com>
2021-01-29 20:27:21 -0800

    Added sample app

---
+++ LogScope/ScopeLogFactory.cs
@@ -0,0 +1,21 @@
+﻿using DevInstance.LogScope.Logger;
+using DevInstance.LogScope.Providers.Console;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevInstance.LogScope
+{
+    public static class ScopeLogFactory
+    {
+        public static IScopeManager Create(LogLevel level, ILogProvider provider)
+        {
+            return new BaseScopeManager(level, provider);
+        }
+
+        public static IScopeManager CreateConsoleLogger(LogLevel level)
+        {
+            return Create(level, new ConsoleLogProvider());
+        }
+    }
+}
---
+++ SampleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using DevInstance.LogScope.Providers.Console;
+using System;
+
+namespace DevInstance.LogScope.SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manager = ScopeLogFactory.Create(LogLevel.DEBUG_EXTRA, new ConsoleLogProvider());
+            var looger = manager.CreateLogger("SampleApp");
+            looger.D("Hello World!");
+            using (var log = looger.DebugExScope())
+            {
+                log.D("Hello from the scope A");
+                using (var sublog = log.DebugExScope("Sub-scope"))
+                {
+                    sublog.D("Hello from the scope B");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
